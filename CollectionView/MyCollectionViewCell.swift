b935149af3bf37707df2a34527965d46b7c4e8e6
Siva Kumar <sivakumar@sivakumars-macbook-air.local>
2018-11-21 11:50:46 +0530

    cv

---
+++ CollectionView/MyCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyCollectionViewCell.swift
+//  CollectionView
+//
+//  Created by sivakumar on 21/11/18.
+//  Copyright © 2018 sivakumar. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imgView: UIImageView!
+}
--- CollectionView/ViewController.swift
+++ CollectionView/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var imgarray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let itemSize = UIScreen.main.bounds.width/3 - 3  // this divides 3 equal items
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 0, bottom: 10, right: 0)
+        layout.itemSize = CGSize(width: itemSize, height: itemSize)
+        layout.minimumInteritemSpacing = 3
+        layout.minimumLineSpacing = 3
+        collectionView.collectionViewLayout = layout
     }
 
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imgarray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MyCollectionViewCell
+        cell.imgView.image = UIImage(named: imgarray[indexPath.item])
+           
+        return cell
+    }
 
 }
 
